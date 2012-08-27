c7d6e06c312a9bcd02b9a802004085da3d09d28b
LuisK <Luisbeckan@hotmail.com>
2012-08-26 19:25:07 -0600

    Update sql/updates/characters/2012_07_15_00_characters_characters.sql
     ( Error )
    DROP TABLE IF EXISTS `character_branchspec`;
    ALTER TABLE `characters` ADD `talentTree` varchar(10) NOT NULL DEFAULT '0 0' AFTER `grantableLevels`;
    ///////////////////////////////////////////////
    'characters'
    [Err] DROP TABLE IF EXISTS `character_branchspec`;
    ALTER TABLE `characters` ADD `talentTree` varchar(10) NOT NULL DEFAULT '0 0' AFTER `grantableLevels`;
    [Msg] Finished - Unsuccessfully
    
    
    
    ( is the repair to the error )
    
    DROP TABLE IF EXISTS `character_branchspec`;
    ALTER TABLE `characters` ADD `grantableLevels` varchar(10) NOT NULL DEFAULT '0 0';
    ALTER TABLE `characters` ADD `talentTree` varchar(10) NOT NULL DEFAULT '0 0' AFTER `grantableLevels`;

--- sql/updates/characters/2012_07_15_00_characters_characters.sql
+++ sql/updates/characters/2012_07_15_00_characters_characters.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS `character_branchspec`;
+ALTER TABLE `characters` ADD `grantableLevels` varchar(10) NOT NULL DEFAULT '0 0';
 ALTER TABLE `characters` ADD `talentTree` varchar(10) NOT NULL DEFAULT '0 0' AFTER `grantableLevels`;
